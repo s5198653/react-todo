101f60626998ef859f9415d86353c7aecb20e05c
Taranovskaya <s5198653@yandex.ru>
2025-04-03 16:49:24 +0300

    create TodoItem component

---
+++ src/app/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import { ITodo } from '../../types/data';
+import styles from './TodoItem.module.css';
+
+interface ITodoItemProps extends ITodo {
+  toggleTodo: (id: number) => void;
+}
+
+export const TodoItem: FC<ITodoItemProps> = (props) => {
+  const { id, title, completed, toggleTodo } = props;
+
+  return (
+    <div className={styles.checkbox__wrapper}>
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => toggleTodo(id)}
+        className={styles.checkbox__control}
+        id={`todo-${id}`}
+      />
+      <label htmlFor={`todo-${id}`} className={styles.checkbox__label}>
+        <span className={styles.checkbox__text}>{title}</span>
+      </label>
+    </div>
+  );
+};
