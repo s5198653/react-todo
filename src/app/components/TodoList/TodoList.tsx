f13ac10969179e89cb06912e387299d304b21481
Taranovskaya <s5198653@yandex.ru>
2025-04-03 16:49:52 +0300

    create TodosList component

---
+++ src/app/components/TodoList/TodoList.tsx
@@ -0,0 +1,86 @@
+'use client';
+import { FC, useState } from 'react';
+import { ITodo } from '../../types/data';
+import styles from './TodoList.module.css';
+import { TodoItem } from '../TodoItem/TodoItem';
+
+interface ITodoListProps {
+  items: ITodo[];
+  removeCompletedTodos: () => void;
+  toggleTodo: (id: number) => void;
+}
+
+export const TodoList: FC<ITodoListProps> = (props) => {
+  const { items, removeCompletedTodos, toggleTodo } = props;
+  const [filter, setFilter] = useState('all');
+
+  const notCompletedCount = items.filter((todo) => !todo.completed).length;
+
+  const filteredItems = () => {
+    switch (filter) {
+      case 'all':
+        return items;
+      case 'active':
+        return items.filter((todo) => !todo.completed);
+      case 'completed':
+        return items.filter((todo) => todo.completed);
+      default:
+        return items;
+    }
+  };
+
+  return (
+    <div className={styles.todos_list}>
+      <h2 className={styles.todos_list__header}>What needs to be done?</h2>
+      <ul className={styles.todos_list__list}>
+        {filteredItems().map((todo) => (
+          <li className={styles.todos_list__item} key={todo.id}>
+            <TodoItem toggleTodo={toggleTodo} {...todo} />
+          </li>
+        ))}
+      </ul>
+      <div className={styles.todos_list__footer}>
+        <p className={styles.todos_list__count}>
+          {notCompletedCount} items left
+        </p>
+        <ul className={styles.todos_list__filters}>
+          <li>
+            <button
+              className={`${styles.todos_list__button} ${
+                filter === 'all' ? styles.active : ''
+              }`}
+              type="button"
+              onClick={() => setFilter('all')}>
+              All
+            </button>
+          </li>
+          <li>
+            <button
+              className={`${styles.todos_list__button} ${
+                filter === 'active' ? styles.active : ''
+              }`}
+              type="button"
+              onClick={() => setFilter('active')}>
+              Active
+            </button>
+          </li>
+          <li>
+            <button
+              className={`${styles.todos_list__button} ${
+                filter === 'completed' ? styles.active : ''
+              }`}
+              type="button"
+              onClick={() => setFilter('completed')}>
+              Completed
+            </button>
+          </li>
+        </ul>
+        <button
+          className={styles.todos_list__button}
+          onClick={removeCompletedTodos}>
+          Clear completed
+        </button>
+      </div>
+    </div>
+  );
+};
